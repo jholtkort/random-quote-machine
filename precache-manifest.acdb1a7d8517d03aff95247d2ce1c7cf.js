7d41dae1834254a5b21ea3bc258d90cd573da43f
Josh Holtkort <jholtkort@utexas.edu>
2019-05-22 14:36:17 -0500

    Updates

---
+++ precache-manifest.acdb1a7d8517d03aff95247d2ce1c7cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf4f1745259e5cd96fe45a500a0cbfcf",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "f5c847492c7370ab8752",
+    "url": "/random-quote-machine/static/css/main.6dc13e07.chunk.css"
+  },
+  {
+    "revision": "d595b206f3f49dbbd917",
+    "url": "/random-quote-machine/static/js/2.4157e642.chunk.js"
+  },
+  {
+    "revision": "f5c847492c7370ab8752",
+    "url": "/random-quote-machine/static/js/main.01baa8dc.chunk.js"
+  },
+  {
+    "revision": "6fefced6fd426e504746",
+    "url": "/random-quote-machine/static/js/runtime~main.296aa1cf.js"
+  }
+]);
